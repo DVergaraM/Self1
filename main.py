0c564ed53969aaea7b3351525b84731da21dfa26
DVergaraM <vergaramartinezdaniel@gmail.com>
2023-07-18 14:55:46 -0500

    Small changes

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!C:/Users/DANIEL/AppData/Local/Programs/Python/Python311/python.exe
-from lib.Pystray import Stray, aries
 import sys
+from typing import NoReturn
+from lib.Pystray import Stray, aries
 # nuitka main.py --windows-icon-from-ico=images/Notion.png --follow-imports --onefile --low-memory --output-filename=MyNotion_No_Console.exe --disable-console
 # C:\Users\DANIEL\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 
 
-def main():
+def main() -> NoReturn:
     
     stray = Stray("Second Brain", aries)
     stray.create_menu()
