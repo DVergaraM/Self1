0ca235c2d741b4bac9be3f0a9c3e11b25d5dbc3d
DVergaraM <vergaramartinezdaniel@gmail.com>
2023-09-28 14:19:51 -0500

    Removed OSThread class

--- src/logic/threads.py
+++ src/logic/threads.py
@@ -5,13 +5,14 @@ from typing import Literal
 import time
 import os
 from . import run_pending
+from PyQt5.QtCore import QProcess
 
 
 class ThreadBase(ABC, threading.Thread):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
         self.stop_event = threading.Event()
-        self.state = "stopped" 
+        self.state = "stopped"
         self.new_thread: threading.Thread = threading.Thread()
 
     @abstractmethod
@@ -52,58 +53,6 @@ class ThreadNotifier(ThreadBase):
             while not self.stop_event.is_set():
                 run_pending()
 
-            self.state = "finished" 
+            self.state = "finished"
         except Exception as e:
             print(f"Error in ThreadNotifier: {e}")
-
-
-class OSThread(ThreadBase):
-    def __init__(self, path: str = None, name: str = None, **kwargs) -> None:
-        super().__init__(**kwargs)
-        self._path = path
-        self._name = name
-        self.cwd = os.getcwd()
-
-    @property
-    def path(self):
-        return self._path
-
-    @path.setter
-    def path(self, value):
-        if isinstance(value, str):
-            self._path = value
-        else:
-            raise TypeError("value must be a string")
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        if isinstance(value, str):
-            self._name = value
-        else:
-            raise TypeError("value must be a string")
-
-    def run(self) -> None:
-        try:
-            date = datetime.now()
-            self.name = "Thread"
-            print(
-                f"[{date.year}-{date.month}-{date.day} {date.hour}:{date.minute}:{date.second}] - {self.name} (run)")
-
-            # while not self.stop_event.is_set():
-            self.path = fr'"{self.path}"'
-            try:
-                os.system(fr"mkdir {self.cwd}\logs\\")
-            except:
-                pass
-            
-            command = fr"{self.path} > {self.cwd}\logs\log-{date.year}-{date.month}-{date.day}_{date.hour}-{date.minute}.log"
-            os.system(command)
-
-            self.state = "finished"
-            self.stop()
-        except Exception as e:
-            print(f"Error in OSThread: {e}")
